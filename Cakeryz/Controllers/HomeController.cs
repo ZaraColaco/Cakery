b202823ea16a8821a3e2f74be563fa71c813c06d
Zara Colaco <ac106503@avcol.school.nz>
2022-04-02 23:57:23 +1300

    2/04/2022 -Spinner added  + Loading screen: Too quick to notice so i have to try and delay the loading of the index page. -Carousel Images have been resized so they are all equal. -More Lorem Ipsum Text Replaced - Nav bar almost done except i cant access the two middle tabs from the last page as they have a different controller.

--- Cakeryz/Controllers/HomeController.cs
+++ Cakeryz/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace Cakeryz.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult OurProducts()
         {
             return View();
         }
