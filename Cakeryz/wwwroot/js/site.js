7dbb939ffa5b77c32d07ce31369893e44303a8a8
Zara Colaco <ac106503@avcol.school.nz>
2022-04-04 01:59:00 +1200

    More animations Added: grow animation on hover added  to the products below the main heading at the top of the page.

--- Cakeryz/wwwroot/js/site.js
+++ Cakeryz/wwwroot/js/site.js
@@ -1,4 +1,58 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿////////function myfunction(){
+////////    $(".loader-wrapper").fadeOut("slow");
+////////}
 
-// Write your JavaScript code.
+////// The loading screen for the website 
+////function Loading(){
+////	$('body').append('<div class= "loader-wrapper"><span class = "loader" > <span class="dot"></span></span ></div >');
+////    Timeout();
+////}
+
+////function hide() {
+////	$(document).ready(function () {
+////		$('.navbar').hide();
+////		$('.main').hide();
+////		//$('body').append('<div class= "loader-wrapper"><span class = "loader" > <span class="dot"></span></span ></div >');
+////		setTimeout(removeLoader, 3700); // Calls Removeloader() after 3700 ms delay
+
+////	}
+////	}
+//// function removeLoader() {
+////			$("#loader-wrapper").fadeOut(500, function () {
+////				$("#loader-wrapper").remove();
+////				$('.navbar').show();
+////				$('.main').show();
+////			})
+////		}
+//<script type="text/javascript">
+//    function OpenNewWindow()
+//    { 
+//            var wdw = window.open("","","toolbar=no,status=no,menubar=no,location=center,scrollbars=no,resizable=no,height=500,width=657");
+//    wdw.document.write("Hello \n\n");
+//    wdw.document.write("yes ");
+//         }
+
+// //$(window).on("load",function(){
+// //     $(".loader-wrapper").fadeOut("slow");
+// //    $('.navbar').hide();
+//	// $('.main').hide();
+// //    $('.footer').hide();
+// //    alert("from header 3");
+//    //});
+// //   alert("from header 2");
+//  </script>
+
+//var winWidth = $(window).width();
+//var winHeight = $(window).height();
+
+//// set initial div height / width
+//$('#page-section').css({
+//    'width': winWidth,
+//});
+
+//// make sure div stays full width/height on resize
+//$(window).resize(function () {
+//    $('#page-section').css({
+//        'width': winWidth,
+//    });
+//});
