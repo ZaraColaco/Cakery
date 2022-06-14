074d7eb058e0f1b8935b2ead2b3704aa297c48a6
Zara Colaco <ac106503@avcol.school.nz>
2022-06-14 14:15:48 +1200

    Login and Regsiter Pages added.

---
+++ Cakeryz/Areas/Identity/Data/CakeryzUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Cakeryz.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the CakeryzUser class
+public class CakeryzUser : IdentityUser
+{
+}
+
